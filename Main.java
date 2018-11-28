cd76c32be7c7b2a708d8bbb59bf97fc056013af1
Julia <midnightwhisper@web.de>
2018-11-28 15:18:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		Scanner eingabe = new Scanner(System.in);
-		Input input = new Input();
-		Header header = input.start(eingabe);
+		Header header = new Header();
+		String output = header.start(header);
 		
-		System.out.println("Soll die Ausgabe binär (0) oder als String (1) erfolgen?");
-		int choice = eingabe.nextInt();
-		eingabe.close();
+		System.out.println(output);
 		
-		int version = header.getVersion();
-		int IHL = header.getIHL();
-		int TOS = header.getTOS();
-		int packetlength = header.getPacketLength();
-		int identifier = header.getIdentifier();
-		String flags = header.getFlags();
-		int offset = header.getOffset();
-		int TTL = header.getTTL();
-		int protocol = header.getProtocol();
-		int checksum = header.getChecksum();
-		String source = header.getSource();
-		String destination = header.getDestination();
-		
-		if(choice == 1) {
-			Object[] headerData = {version, IHL, TOS, packetlength ,identifier, flags, offset, TTL, protocol, checksum, source, destination};
-			System.out.println(buildstring(headerData));	
-		} else {
-			// Binary-Method here!
-		}
-		
-	}
-	
-	private static String buildstring(Object[] headerData) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for(int i = 0; i< headerData.length; i++){
-			stringBuilder.append(headerData[i]);
-			stringBuilder.append("-");
-		}
-		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-		String headerString = stringBuilder.toString();
-		return headerString;
 	}
 }
